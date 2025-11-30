e2affb1fda31b9469e19f3e3b132b9dd86d0c125
Ron Wettenstein <ron.wettenstein@gmail.com>
2025-11-30 18:47:22 +0200

    fix rm

--- deploy_new_version.bash
+++ deploy_new_version.bash
@@ -9,7 +9,7 @@ python -c "import os; os.chdir('..'); os.chdir('..'); import woodelf; print(wood
 # Build the package
 #If needed: python -m pip install --upgrade build twine
 windows: del .\dist\*
-linux: del .\dist\*
+linux: rm .\dist\*
 
 python -m build
 # see files were created in dist/
