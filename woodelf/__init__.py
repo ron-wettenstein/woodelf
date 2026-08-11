cf184ac399b9700d6485b05a1dc098357034df88
Ron Wettenstein <ron.wettenstein@gmail.com>
2026-08-11 14:43:02 +0300

    new version

--- woodelf/__init__.py
+++ woodelf/__init__.py
@@ -3,4 +3,4 @@ import woodelf.simple_woodelf
 from woodelf.explainer import WoodelfExplainer
 from woodelf.pdp_display import WoodelfPartialDependenceDisplay
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
